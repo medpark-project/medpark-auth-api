f87cd40ff367414ebc009f4a8261115c539ab01a
Brunna <brunna.louisemr@gmail.com>
2025-10-22 17:36:56 -0300

    refactor: remove logica de usuario

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from src.db.session import Base, engine
-from src.usuario.router import router as usuario_router
 
 
 @asynccontextmanager
@@ -13,7 +12,6 @@ async def lifespan(app: FastAPI):
 
 def create_app() -> FastAPI:
     app = FastAPI(title="MedPark Auth API", version="0.1.0", lifespan=lifespan)
-    app.include_router(usuario_router, prefix="/usuarios", tags=["Usuarios"])
     return app
 
 
--- src/security.py
+++ src/security.py
@@ -1,19 +1,10 @@
 from datetime import datetime, timedelta, timezone
 
-import bcrypt
 from jose import jwt
 
 SECRET_KEY = "sua-chave-secreta-super-dificil-de-adivinhar"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def verify_password(plain_password: str, hashed_password: str) -> bool:
-    password_bytes = plain_password.encode("utf-8")
-    hashed_password_bytes = hashed_password.encode("utf-8")
-    return bcrypt.checkpw(
-        password=password_bytes, hashed_password=hashed_password_bytes
-    )
 
 
 def create_access_token(data: dict) -> str:
